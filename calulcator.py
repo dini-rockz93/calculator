5862c90a1758e9e823152f6a75180354d782d64f
dsg <vijayadinesh93@gmail.com>
2019-12-15 13:14:34 +0530

    calculator function

---
+++ calulcator.py
@@ -0,0 +1,14 @@
+def addition(a, b):
+    """
+    Input: a, b
+    Output: a + b
+    """
+    return a + b
+
+def subtraction(a, b):
+    """
+    Input: a, b
+    Output: a - b
+    """
+    
+    return a - b
